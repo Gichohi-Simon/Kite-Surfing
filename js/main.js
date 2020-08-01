51952e780202a3af277b631949597e3462b02f9f
Gichohi <njogugichohi56@gmail.com>
2020-08-01 16:37:34 +0300

    fixed everything including media queries, except lessons

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+/*weather javascript */
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];
+if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';
+fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+
 /*contact form validation  */
 function validateForm() {
     var name =  document.getElementById('name').value;
@@ -28,3 +33,4 @@ function validateForm() {
     }
     document.querySelector('.status').innerHTML = "Sending...";
   }
+
