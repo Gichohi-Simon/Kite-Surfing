c5de5d5860c970db8ff2920aa865282dd7483312
Gichohi <njogugichohi56@gmail.com>
2020-08-06 19:56:53 +0300

    loading the first face

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ function validateForm() {
         document.querySelector('.status').innerHTML = "Message cannot be empty";
         return false;
     }
-    document.querySelector('.status').innerHTML = "Sending...";
+    document.querySelector('.status').innerHTML = "form under construction...";
   }
     // Get the modal
 
